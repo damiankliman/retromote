4866089463f54de878b1d47a573d97c49c16c372
damiankliman <klimandamian@gmail.com>
2023-04-15 13:58:13 -0700

    add fontawesomeicons

--- components/ConnectionBar.tsx
+++ components/ConnectionBar.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faCircleDot } from "@fortawesome/free-solid-svg-icons";
 
 const ConnectionBar = () => {
   return (
     <View className="py-4 px-4 items-center flex-row gap-3 border-b">
-      <Text>O</Text>
+      <FontAwesomeIcon icon={faCircleDot} />
       <View className="flex-row justify-between flex-1">
         <Text>Tap to connect...</Text>
         <Text>-7</Text>
