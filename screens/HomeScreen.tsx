d466a3ae5c583847ed69f95934248cd1ebfaeb27
damiankliman <klimandamian@gmail.com>
2023-04-15 15:51:57 -0700

    add mode buttons

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,13 +1,37 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Text, View } from "react-native";
 import ConnectionBar from "../components/ConnectionBar";
+import ModeButton from "../components/ModeButton";
+
+const modes = [
+  {
+    text: "M",
+    onPress: () => {
+      console.log("Manual mode");
+    },
+  },
+  {
+    text: "B",
+    onPress: () => {
+      console.log("Bulb mode");
+    },
+  },
+  {
+    text: "T",
+    onPress: () => {
+      console.log("Bulb timer mode");
+    },
+  },
+];
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView className="bg-white">
+    <SafeAreaView className="h-full bg-white">
       <ConnectionBar />
-      <View className="h-full">
-        <Text>HomeScreen</Text>
+      <View className="w-full flex-grow flex-col items-center justify-center">
+        {modes.map((mode, index) => (
+          <ModeButton key={index} text={mode.text} onPress={mode.onPress} />
+        ))}
       </View>
     </SafeAreaView>
   );
