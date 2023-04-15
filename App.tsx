a8e17082e335eeabf52c22202ecf114d8211300e
damiankliman <klimandamian@gmail.com>
2023-04-15 12:48:17 -0700

    remove old styles

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text className="font-bold">
         Open up App.tsx to start working on your app!
       </Text>
@@ -11,12 +11,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
