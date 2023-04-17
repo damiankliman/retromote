0b714761abedfe98e0e2cc295060cae7fc79da3b
damiankliman <klimandamian@gmail.com>
2023-04-16 20:15:10 -0700

    handle shutter press and press style

--- screens/ManualScreen.tsx
+++ screens/ManualScreen.tsx
@@ -5,12 +5,31 @@ import { faCameraRetro } from "@fortawesome/free-solid-svg-icons";
 import HeaderBar from "../components/HeaderBar";
 
 const ManualScreen = () => {
+  const handleShutterPress = () => {
+    console.log("Pressed");
+  };
+
   return (
     <SafeAreaView className="h-full bg-white">
       <HeaderBar screenTitle="Manual" />
       <View className="flex-grow items-center justify-center">
-        <Pressable className="h-64 w-64 items-center justify-center rounded-full bg-gray-100">
-          <FontAwesomeIcon icon={faCameraRetro} size={100} color="#A1A1AA" />
+        <Pressable
+          className="rounded-full"
+          onPressIn={() => handleShutterPress()}
+        >
+          {({ pressed }) => (
+            <View
+              className={`h-64 w-64 items-center justify-center rounded-full bg-gray-100 ${
+                pressed && "opacity-70"
+              }`}
+            >
+              <FontAwesomeIcon
+                icon={faCameraRetro}
+                size={100}
+                color="#A1A1AA"
+              />
+            </View>
+          )}
         </Pressable>
       </View>
     </SafeAreaView>
