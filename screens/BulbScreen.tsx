b38eaf325b9ce9e6410f927ffc3eb95d574a0592
damiankliman <klimandamian@gmail.com>
2023-04-16 20:22:28 -0700

    add header to bulb and timer screens

--- screens/BulbScreen.tsx
+++ screens/BulbScreen.tsx
@@ -1,10 +1,13 @@
 import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import HeaderBar from "../components/HeaderBar";
 
 const BulbScreen = () => {
   return (
-    <View>
-      <Text>ManualScreen</Text>
-    </View>
+    <SafeAreaView className="h-full bg-white">
+      <HeaderBar screenTitle="Bulb" />
+      <Text>BulbScreen</Text>
+    </SafeAreaView>
   );
 };
 export default BulbScreen;
--- screens/TimerScreen.tsx
+++ screens/TimerScreen.tsx
@@ -1,10 +1,13 @@
 import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import HeaderBar from "../components/HeaderBar";
 
 const TimerScreen = () => {
   return (
-    <View>
-      <Text>ManualScreen</Text>
-    </View>
+    <SafeAreaView className="h-full bg-white">
+      <HeaderBar screenTitle="Timer" />
+      <Text>TimerScreen</Text>
+    </SafeAreaView>
   );
 };
 export default TimerScreen;
