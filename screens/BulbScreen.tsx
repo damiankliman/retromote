cf2e60b5c92864aefdd70a8465511c3f2a10fbde
damiankliman <klimandamian@gmail.com>
2023-04-16 21:09:37 -0700

    use arrow buttons to inc and dec timer

--- screens/BulbScreen.tsx
+++ screens/BulbScreen.tsx
@@ -20,7 +20,11 @@ const BulbScreen = () => {
       <HeaderBar screenTitle="Bulb" />
       <View className="flex-grow items-center justify-center">
         <View className="flex-row items-center">
-          <Pressable>
+          <Pressable
+            onPressIn={() =>
+              timerValue > 0 ? setTimerValue(timerValue - 1) : null
+            }
+          >
             <FontAwesomeIcon icon={faCaretLeft} size={120} color="#A1A1AA" />
           </Pressable>
           <View className="relative">
@@ -31,13 +35,14 @@ const BulbScreen = () => {
               }}
               inputMode="numeric"
               textAlign="center"
+              returnKeyType="done"
               className="h-20 w-36 items-center justify-center rounded-xl bg-gray-200 text-center text-5xl font-semibold leading-[55px] text-gray-500"
             />
             <Text className="absolute bottom-1 right-2 font-semibold text-gray-400">
               S
             </Text>
           </View>
-          <Pressable>
+          <Pressable onPressIn={() => setTimerValue(timerValue + 1)}>
             <FontAwesomeIcon icon={faCaretRight} size={120} color="#A1A1AA" />
           </Pressable>
         </View>
